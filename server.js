e267afd977b1e44bdb33108d43104e9f1e00ca48
ProfCarlos <hernandeznc@ipn.mx>
2021-03-19 15:33:27 -0600

    modificacion parámetro nombre

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-  res.send("Hola Martin y Jessica");
+  res.send("Hola Martin y Jessica ... y "+ req.query.nombre);
 });
 
 const port = process.env.PORT || 8080;
@@ -11,5 +11,3 @@ const port = process.env.PORT || 8080;
 app.listen(8080, () => {
     console.log("hola desde la consola 2");
 });
-
-
